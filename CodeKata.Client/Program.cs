7e1faa31377bf579a5cac71f7cf782ac04f629e3
s.griffin <s.griffin@controlexpert.com>
2023-12-08 08:25:12 -0600

    Add project files.

---
+++ CodeKata.Client/Program.cs
@@ -0,0 +1,12 @@
+using Core.Api;
+using Core.Network.Client;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+NetworkServiceOptions networkOptions = builder.Configuration.GetOptions<NetworkServiceOptions>(services, "NetworkServices");
+builder.Services.AddApis<BaseAddressAuthorizationMessageHandler>(typeof(IGridDataApi).Assembly, apiUrl);
+
+await builder.Build().RunAsync();
